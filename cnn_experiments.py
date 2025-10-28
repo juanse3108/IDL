0ff473392c9375042b4d4bf4ca51f7cd7a4bb72a
juanse3108 <sebastianmmelendez@gmail.com>
2025-10-28 10:32:28 +0100

    experiments 99-150

--- cnn_experiments.py
+++ cnn_experiments.py
@@ -49,8 +49,8 @@ param_combinations = list(itertools.product(
 ))
 
 # Subtesting combinations of parameters to avoid crashin due to GPU memory limits
-start = 60
-end = 100
+start = 99
+end = 150
 param_combinations = param_combinations[start:end]
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
